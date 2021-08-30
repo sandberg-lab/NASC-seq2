eca601f367ece77c642f07777437c8d331c41d2c
Anton Larsson <antonlarsson23@gmail.com>
2021-08-30 15:03:27 +0200

    Re-wrote do_htest.py to be more efficient

--- do_htest.py
+++ do_htest.py
@@ -44,16 +44,26 @@ def get_c_and_p(n, p_c, p_e, a):
 
 def cell_new_htest(cell_id, h5file, a, q):
     fd = h5py.File(h5file, 'r')
-    grp = fd['cells/{}'.format(cell_id)]
-    p_c = grp.attrs['p_c']
-    p_e = grp.attrs['p_e']
+    cell_grp = fd['cells/{}'.format(cell_id)]
+    p_c = cell_grp.attrs['p_c']
+    p_e = cell_grp.attrs['p_e']
     if p_c < p_e:
         p_c = p_e + 1e-5
-    gene_array = grp['gene'][:]
-    sc = grp['sc'][:]
-    len_to_split = [len(c) for c in sc]
+    cell_id_bstring = cell_id.encode()
+    gene_list = []
+    sc_list = []
+    tc_list = []
+    for gene, gene_grp in fd['genes'].items():
+        cell_array =  gene_grp['cell'][:]
+        if cell_id_bstring in cell_array:
+            gene_list.append(gene)
+            loc = np.where(cell_id_bstring == cell_array)
+            sc_list.append(gene_grp['sc'][loc])
+            tc_list.extend(gene_grp['tc'][loc])
+    gene_array = np.array(gene_list)
+    len_to_split = [len(c) for c in sc_list]
     sc_array = np.concatenate(sc)
-    tc_array = np.concatenate(grp['tc'][:])
+    tc_array = np.array(tc_list)
     fd.close()
 
     pc_log = binom.logpmf(sc_array, tc_array, p_c)
@@ -67,6 +77,7 @@ def cell_new_htest(cell_id, h5file, a, q):
     c_array = np.array([m[0] for m in criteria_list])
     q_array = np.array([m[1] for m in criteria_list])
     htest_array = do_htest(rel_ll, c_array, q_array)
+    
     res_dict = {}
     res_dict['gene'] = gene_array
     res_dict['htest'] = split_list(htest_array, len_to_split)
@@ -74,8 +85,20 @@ def cell_new_htest(cell_id, h5file, a, q):
     res_dict['c_htest'] = split_list(c_array, len_to_split)
     res_dict['q_htest'] = split_list(q_array, len_to_split)
     res_dict['power'] = split_list(power_array, len_to_split)
-    fd.close()
-    q.put((cell_id, res_dict))
+    
+    f_h5_cell = h5py.File('{}_tmp.h5'.format(cell_id),'w')
+    for k,v in res_dict.items():
+        if k == 'gene':
+            f_h5_cell.create_dataset(tag, data=np.array(v, dtype='S'))
+        elif tag=='htest':
+            dset = f_h5_cell.create_dataset(k, (len(v),), dtype=h5py.vlen_dtype(np.dtype('bool')))
+            for i, arr in enumerate(v):
+                dset[i] = arr
+        else:
+            dset = f_h5_cell.create_dataset(k, (len(v),), dtype=h5py.vlen_dtype(np.dtype('float32')))
+            for i, arr in enumerate(v):
+                dset[i] = arr
+    q.put(cell_id)
     return cell_id
 
 
@@ -83,41 +106,70 @@ def split_list(l, len_to_split):
     return [l[x - y: x] for x, y in zip(accumulate(len_to_split), len_to_split)]
 
 
-def make_write_function(h5file):
+def make_write_cell_function(h5file):
     def write_hdf5(q):
-        full_gene_dict = {}
+        f_h5 = h5py.File(h5file, 'a')
         while True:
-            cell_id, cell_dict = q.get()
-            if cell_dict is None:
+            cell_id = q.get()
+            if cell_id is None:
                 break
-            for gene, htest, rel_ll, c_htest, q_htest, power in zip(*cell_dict.values()):
-                gene = gene.decode('utf-8')
-                if gene in full_gene_dict:
-                    full_gene_dict[gene][cell_id] = {'htest': htest, 'rel_ll': rel_ll, 'c_htest': c_htest, 'q_htest': q_htest, 'power': power}
-                else:
-                    full_gene_dict[gene] = {}
-                    full_gene_dict[gene][cell_id] = {'htest': htest, 'rel_ll': rel_ll, 'c_htest': c_htest, 'q_htest': q_htest, 'power': power}
-            del cell_dict
+            f_h5_cell = h5py.File('{}_tmp.h5'.format(cell_id),'r')
+            cell_grp = f_h5['cells/{}'.format(cell_id)]
+            for key in f_h5_cell.keys():
+                f_h5_cell.copy(key, cell_grp)
+            f_h5_cell.close()
+            os.system('rm {}_tmp.h5'.format(cell_id))
             q.task_done()
-        data_to_write = {}
-        hdf5_file = h5py.File(h5file, 'a')
-        genes_grp = hdf5_file['genes']
-        for gene, gene_grp in genes_grp.items():
-            data_to_write = {'htest': [], 'rel_ll': [], 'c_htest': [], 'q_htest': [], 'power': []}
-            specific_gene_dict = full_gene_dict[gene]
-            for cell_id in gene_grp['cell'][:]:
-                cell_id = cell_id.decode('utf-8')
-                for k in data_to_write.keys():
-                    data_to_write[k].append(specific_gene_dict[cell_id][k])
-            for k, v in data_to_write.items():
-                if k == 'htest':
-                    dset = gene_grp.create_dataset(k, (len(v),), dtype=h5py.vlen_dtype(np.dtype('bool')))
-                else:
-                    dset = gene_grp.create_dataset(k, (len(v),), dtype=h5py.vlen_dtype(np.dtype('float32')))
-                for i, arr in enumerate(v):
-                    dset[i] = arr
-        hdf5_file.close()
+        f_h5.close()
         q.task_done()
+    return write_hdf5
+
+def compile_gene_info(gene_id, h5file, q):
+    fd = h5py.File(h5file, 'r')
+    gene_id_bstring = gene_id.encode()
+    
+    gene_dict = {}
+    for cell_id, cell_grp in fd['cells'].items():
+        gene_array = cell_grp['gene'][:]
+        if gene_id_bstring in gene_array:
+            loc = np.where(gene_id_bstring == gene_array)
+            gene_dict[cell_id] = {'htest': cell_grp['htest'][loc], 'rel_ll': cell_grp['rel_ll'][loc],'c_htest': cell_grp['c_htest'][loc],'q_htest': cell_grp['q_htest'][loc],'power': cell_grp['power'][loc]}
+    f_out = h5py.File('{}_tmp.h5'.format(gene_id), 'w')
+    gene_grp = fd['genes/{}'.format(gene_id)]
+    data_to_write =  {'htest': [], 'rel_ll': [], 'c_htest': [], 'q_htest': [], 'power': []}
+    cells = gene_grp['cell'][:]
+    for cell_id in cells:
+        cell_id = cell_id.decode('utf-8')
+        for k in data_to_write.keys():
+            data_to_write[k].append(gene_dict[cell_id][k])
+    for k, v in data_to_write.items():
+        if k == 'htest':
+            dset = f_out.create_dataset(k, (len(v),), dtype=h5py.vlen_dtype(np.dtype('bool')))
+        else:
+            dset = f_out.create_dataset(k, (len(v),), dtype=h5py.vlen_dtype(np.dtype('float32')))
+        for i, arr in enumerate(v):
+            dset[i] = arr
+    f_out.close()
+    q.put(gene_id)
+    return gene_id
+
+
+
+def make_write_gene_function(h5file):
+    def write_hdf5(q):
+        hdf5_file = h5py.File(h5file, 'a')
+        while True:
+            gene_id = q.get()
+            if gene_id is None:
+                break
+            f_h5_gene = h5py.File('{}_tmp.h5'.format(gene_id),'r')
+            gene_grp = hdf5_file['genes/{}'.format(gene_id)]
+            for key in f_h5_gene.keys():
+                f_h5_gene.copy(key, gene_grp)
+            f_h5_cell.close()
+            q.task_done()
+    hdf5_file.close()
+    q.task_done()
     return write_hdf5
 
 
@@ -128,15 +180,28 @@ if __name__ == '__main__':
     args = parser.parse_args()
     h5file = args.hdf5
     threads = args.threads
-    print('Doing hypothesis test for new RNA')
+    
+    print('Doing hypothesis test for new RNA, cell level')
     fd = h5py.File(h5file, 'r')
     cell_list = list(fd['cells'].keys())
+    gene_list = list(fd['genes'].keys())
     fd.close()
     m = Manager()
     q = m.JoinableQueue()
-    p = Process(target=make_write_function(h5file), args=(q,))
+    p = Process(target=make_write_cell_function(h5file), args=(q,))
     p.start()
     res = Parallel(n_jobs=threads, verbose=3, backend='loky')(delayed(cell_new_htest)(cell_id, h5file, 0.01, q) for cell_id in cell_list)
-    q.put((None, None))
-    print('Done, writing results')
+    q.put(None)
     p.join()
+
+    print('Compiling information on gene level')
+
+    m = Manager()
+    q = m.JoinableQueue()
+    p = Process(target=make_write_gene_function(h5file), args=(q,))
+    p.start()
+    res = Parallel(n_jobs=threads, verbose=3, backend='loky')(delayed(compile_gene_info)(gene_id, h5file, q) for gene_id in gene_list)
+    q.put(None)
+    p.join()
+
+    print('Done.')
